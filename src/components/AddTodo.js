039a44f92cb987b7a77719d2ffa010d0ad340b22
patlub <patricklu2010@gmail.com>
2018-04-17 23:47:53 +0300

    implement addTodo component

---
+++ src/components/AddTodo.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import AddTodoForm from '../containers/AddTodoForm';
+import { addTodoAction } from '../actions/auth';
+import ApplicationBar from './ApplicationBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const mapStateToProps = state => ({ state });
+
+class AddTodo extends React.Component {
+
+  render() {
+    const { addTodoAction } = this.props;
+    return (
+      <div className="container-fluid">
+        <MuiThemeProvider>
+          <div>
+            <div className="row">
+              <ApplicationBar />
+            </div>
+            <div className="row vertical">
+              <div className="col-3">
+                <div className="col-12 sign-up-title">Add Todo</div>
+                <AddTodoForm signinAction={addTodoAction} />
+              </div>
+            </div>
+          </div>
+        </MuiThemeProvider>
+      </div>
+
+    );
+  }
+}
+
+AddTodo.propTypes = {
+  addTodoAction: PropTypes.func,
+};
+
+export default connect(mapStateToProps, { addTodoAction })(AddTodo);
